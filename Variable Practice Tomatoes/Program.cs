2fb8b90d715b98bfd58b006b8cb45ed2d2f75f97
A.J. Miller <ajmiller1206@gmail.com>
2022-09-12 11:53:57 -0400

    wrote some code with some text about tomatoes

---
+++ Variable Practice Tomatoes/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string crop = "tomato";
+            int numPlants = 5;
+            int acres = 15;
+            char firstChar = 'l';
+            char secondChar = 'o';
+            char thirdChar = 'n';
+            char fourthChar = 'g';
+            bool isRipe = false;
+            double growingPeriod = 1.5;
+            const decimal sfInAcre = 43560;
+
+            Console.WriteLine($"We have been waiting a {firstChar}{secondChar}{thirdChar}{fourthChar} time to grow plants.");
+            Console.WriteLine($"This year, we have planted {numPlants} {crop} plants.");
+            Console.WriteLine($"Are the tomatoes ready? {isRipe}");
+            Console.WriteLine($"We have to wait {growingPeriod} months for the first {crop}es.");
+            Console.WriteLine($"We have {acres} acres of land; that's {sfInAcre} feet per acre.");
+            Console.WriteLine($"In total, we have {acres * sfInAcre} square feet to work with.");
+        }
+    }
+}
